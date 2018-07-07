e1dc3043d34fdd1844fe17a61154361e8e51ed99
TheoCapon <captheo@hotmail.fr>
2018-07-07 19:03:17 +0200

    Connexion base

--- src/model/ConnexionBDD.java
+++ src/model/ConnexionBDD.java
@@ -1,5 +1,53 @@
 package model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class ConnexionBDD {
 
+	public static void main(String[] args) {
+		 Connection con = null;
+		 ResultSet rs = null;
+		 PreparedStatement pstmt = null;
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+           
+            con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:ROOT","admin","azerty");
+            pstmt = con.prepareStatement("SELECT * FROM CLIENT");
+            rs = pstmt.executeQuery();
+            while(rs.next()) {
+            	System.out.println(rs.getString(2));
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+            return;
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (pstmt != null) {
+                try {
+                    pstmt.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (con != null) {
+                try {
+                    con.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        
+    }
+	
 }
